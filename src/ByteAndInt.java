dccad374b9f3366389ad6eaa2fbc7494d4b5369d
gowthamselvarajgit <gowtham26.work@gmail.com>
2024-09-28 15:18:22 +0530

    first commit

---
+++ src/ByteAndInt.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ByteAndInt {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        int num = 257;
+        byte changer =(byte)(num);
+        System.out.println(changer);
+        /*
+         we will get an value 1 because the maximum size of the byte if 256 and we are giving larger than that so it make
+         an difference between given number and the size of byte that is 256.
+         given number is 257
+         size of the byte is 256
+         257-256 = 1
+
+         */
+
+    }
+}
