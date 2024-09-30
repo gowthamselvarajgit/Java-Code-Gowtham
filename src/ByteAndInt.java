be944807c92bd5f71563b90a8426cbe147c1e343
gowthamselvarajgit <gowtham26.work@gmail.com>
2024-09-30 06:08:19 +0530

    second commit

--- src/ByteAndInt.java
+++ src/ByteAndInt.java
@@ -6,6 +6,11 @@ public class ByteAndInt {
         int num = 257;
         byte changer =(byte)(num);
         System.out.println(changer);
+        byte number1 = 20;
+        byte number2 = 30;
+        byte number3 = 10;
+        int result = number1 * number2 / number3;
+        System.out.println(result);
         /*
          we will get an value 1 because the maximum size of the byte if 256 and we are giving larger than that so it make
          an difference between given number and the size of byte that is 256.
