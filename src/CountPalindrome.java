0fa41a02b61d6b7285e8e20f8dd09ee99e6fc548
gowthamselvarajgit <gowtham26.work@gmail.com>
2024-10-03 17:51:53 +0530

    first commit

---
+++ src/CountPalindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class CountPalindrome {
+    public static boolean checkPalindrome(int num){
+        int copy = num;
+        int rev = 0;
+        while(copy != 0){
+            int digit = copy % 10;
+            rev = rev * 10 + digit;
+            copy = copy/10;
+        }
+        if(rev == num){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int count = 0;
+        for(int begin = start; begin < end; begin+=1){
+            if(checkPalindrome(begin)){
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
