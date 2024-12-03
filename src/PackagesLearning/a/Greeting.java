907f6dc50acd8cc4e0c0dc59a3765cd860d6aa91
gowthamselvarajgit <gowtham26.work@gmail.com>
2024-12-03 13:31:26 +0530

    first commit

---
+++ src/PackagesLearning/a/Greeting.java
@@ -0,0 +1,9 @@
+package PackagesLearning.a;
+
+import static PackagesLearning.b.Greeting.greeter;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeter();
+    }
+}
---
+++ src/PackagesLearning/b/Greeting.java
@@ -0,0 +1,10 @@
+package PackagesLearning.b;
+
+public class Greeting {
+    public static void greeter(){
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+
+    }
+}
